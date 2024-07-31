8eec79f730e9150014e36ef1f5f0b8734bad6c02
Vaida <developer@vaidaf.net>
2024-07-31 22:48:23 +1000

    updates testings

--- Tests/MetalManagerTests/MetalManagerTests.swift
+++ Tests/MetalManagerTests/MetalManagerTests.swift
@@ -8,19 +8,7 @@ struct MetalManagerTests {
     
     @Test
     func textureConversion() async throws {
-        let image = NSImage(systemSymbolName: "doc", accessibilityDescription: nil)!
-        let sourceImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
-        let context = CGContext(
-            data: nil,
-            width: sourceImage.width,
-            height: sourceImage.height,
-            bitsPerComponent: 8,
-            bytesPerRow: 4 * sourceImage.width,
-            space: CGColorSpaceCreateDeviceRGB(),
-            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
-        )!
-        context.draw(sourceImage, in: CGRect(origin: .zero, size: CGSize(width: sourceImage.width, height: sourceImage.height)))
-        let cgImage = context.makeImage()!
+        let cgImage = makeSampleCGImage()
         
         let device = MetalManager.Configuration.shared.computeDevice
         let texture = try device.makeTexture(from: cgImage, usage: .shaderRead)
@@ -78,4 +66,21 @@ struct MetalManagerTests {
         try await #expect(result.synchronize() == false)
     }
     
+}
+
+
+private func makeSampleCGImage() -> CGImage {
+    let image = NSImage(systemSymbolName: "doc", accessibilityDescription: nil)!
+    let sourceImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+    let context = CGContext(
+        data: nil,
+        width: sourceImage.width,
+        height: sourceImage.height,
+        bitsPerComponent: 8,
+        bytesPerRow: 4 * sourceImage.width,
+        space: CGColorSpaceCreateDeviceRGB(),
+        bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+    )!
+    context.draw(sourceImage, in: CGRect(origin: .zero, size: CGSize(width: sourceImage.width, height: sourceImage.height)))
+    return context.makeImage()!
 }
